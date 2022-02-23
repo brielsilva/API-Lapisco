5d5c132479ddc8e621808bcd4e0cefef242d251b
Gabriel Costa <gabrielcostasilva100@gmail.com>
2022-02-23 04:02:16 -0300

    add to table createdAt and updatedAt, implement the find's method of the contact repository, needs testing

--- db/migrate/migration.js
+++ db/migrate/migration.js
@@ -1,7 +1,7 @@
 const db = require('../db');
 
-db.query('DROP TABLE contacts;');
-db.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`)
+db.query('DROP TABLE IF EXISTS contacts');
+db.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
 db.query(`CREATE TABLE IF NOT EXISTS contacts(
 	id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
 	email VARCHAR NOT NULL UNIQUE,
@@ -9,7 +9,10 @@ db.query(`CREATE TABLE IF NOT EXISTS contacts(
 	phone VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
 	birthday DATE NOT NULL,
-	picture VARCHAR NOT NULL
+	picture VARCHAR NOT NULL,
+	createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );`);
 
+db.query(`INSERT INTO contacts(name,email,gender,phone,picture,birthday) VALUES('gabriel',"gabrielcostasilva100@gmail.com","M","85985892063","ALGUMAHOSTAQUI","2020-01-20");`);
 
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -3,9 +3,18 @@ const db = require('../../../db/db');
 class ContactsRepository {
 	create({name, email, phone, gender, picture, birthday}) {}
 	findById(id) {}
-	findAll() {}
-	findByName() {}
-	findByEmail() {}
+	async findAll() {
+		const rows = await db.query('SELECT * FROM contacts');
+		return rows;
+	}
+	async findByName(name) {
+		const rows = await db.query(`SELECT * FROM contacts WHERE LOWER($1) LIKE $2`,[name,name],(err))
+		return rows;
+	}
+	async findByEmail(email) {
+		const rows = await db.query('SELECT * FROM contacts WHERE email = $1',[email]);
+		return rows;
+	}
 	update(id) {}
 	delete(id) {}
 }
