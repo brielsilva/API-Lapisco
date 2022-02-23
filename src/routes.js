246673c3f9d265f551742a20b9ad8eb41bfda4e6
Gabriel Costa <gabrielcostasilva100@gmail.com>
2022-02-23 02:53:03 -0300

    create base routes for the api

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,13 @@
 const { Router } = require('express');
+const ContactController = require('./app/controllers/ContactController');
 
 const routes = Router();
+
+routes.get('/contacts',ContactController.index);
+routes.get('/contacts/:id',ContactController.show);
+routes.post('/contacts',ContactController.store);
+routes.put('/contacts/:id',ContactController.update);
+routes.delete('/contacts/:id',ContactController.delete);
 
 
 module.exports = routes;
